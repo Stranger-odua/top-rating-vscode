d80b4e3128909f28b2193fc4ceb2c30271542a90
Stranger <rooderuler@gmail.com>
2021-07-11 17:33:33 +0300

    06:17 005 Работа с svg - с простым вариантом работы с svg

--- next.config.js
+++ next.config.js
@@ -1,3 +1,13 @@
 module.exports = {
-  reactStrictMode: true,
-}
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/, //если мы видим файл свг
+      issuer: {
+        //в файлах дж, тс, тсх
+        test: /\.(js|ts)x?$/,
+      },
+      use: ["@svgr/webpack"], // то мы используем специальный загрузчик
+    });
+    return config;
+  },
+};
